85ba69f07c5e3cbba7bcdf99b07bf10092bad1d4
= <=>
2023-05-01 10:59:38 +0700

    Created Start/Stop Parking feature

--- app/Models/Parking.php
+++ app/Models/Parking.php
@@ -2,8 +2,11 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Parking extends Model
 {
@@ -22,4 +25,31 @@ class Parking extends Model
         'start_time' => 'datetime',
         'stop_time' => 'datetime',
     ];
+
+    protected static function booted()
+    {
+        static::addGlobalScope('user', function (Builder $builder) {
+            $builder->where('user_id', auth()->id());
+        });
+    }
+
+    public function zone(): BelongsTo
+    {
+        return $this->belongsTo(Zone::class);
+    }
+
+    public function vehicle(): BelongsTo
+    {
+        return $this->belongsTo(Vehicle::class);
+    }
+
+    public function scopeActive($query)
+    {
+        return $query->whereNull('stop_time');
+    }
+
+    public function scopeStopped($query)
+    {
+        return $query->whereNotNull('stop_time');
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\V1\ParkingController;
 use App\Http\Controllers\Api\V1\VehicleController;
 use App\Http\Controllers\Api\V1\ZoneController;
 use Illuminate\Http\Request;
@@ -21,16 +22,30 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+/**
+ * Routes for user's auth
+ */
 Route::post('auth/register', Auth\RegisterController::class);
 Route::post('auth/login', Auth\LoginController::class);
 Route::post('auth/logout', Auth\LogoutController::class);
 
 Route::middleware('auth:sanctum')->group(function () {
+    /**
+     * Routes for profile and password update
+     */
     Route::get('profile', [Auth\ProfileController::class, 'show']);
     Route::put('profile', [Auth\ProfileController::class, 'update']);
     Route::put('password', Auth\PasswordUpdateController::class);
 
     Route::apiResource('vehicles', VehicleController::class);
+
+    /**
+     * Routes for parkings
+     */
+    Route::post('parkings/start', [ParkingController::class, 'start']);
+    Route::get('parkings/{parking}', [ParkingController::class, 'show']);
+    Route::put('parkings/{parking}', [ParkingController::class, 'stop']);
+
 });
 
 Route::get('zones', [ZoneController::class, 'index']);
